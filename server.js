3b7b53a749074d7340e2ba74b213f8f1c2dd138b
GihanPasidu <sahanrowdy@gmail.com>
2025-03-08 09:53:53 +0530

    update

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const jsonServer = require("json-server");
-const app1 = jsonServer.create();
-const app2 = jsonServer.create();
-const router1 = jsonServer.router("db.json");
-const router2 = jsonServer.router("user.json");
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-app1.use(middlewares);
-app1.use(router1);
-app2.use(middlewares);
-app2.use(router2);
+const PORT = process.env.PORT || 5000; // Render will assign a dynamic port
 
-const PORT1 = process.env.PORT || 5000;
-const PORT2 = 5001;
+app.use(middlewares);
+app.use(router);
 
-app1.listen(PORT1, () => {
-  console.log(`DB Server running on port ${PORT1}`);
-});
-
-app2.listen(PORT2, () => {
-  console.log(`User Server running on port ${PORT2}`);
+app.listen(PORT, () => {
+  console.log(`Shoes API running on port ${PORT}`);
 });
